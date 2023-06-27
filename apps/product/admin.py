b28fc9659a163b3f638225505ce39d853da7dafb
Romazan <zalalidinovroma@gmail.com>
2023-06-27 18:19:42 +0600

    changing admin site

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.product.models import Product, ProductImage
+
+admin.site.register(ProductImage)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    inlines = (ProductImageInline,)
+
